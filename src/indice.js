dfe37adc38d20054c16c55028e97494bc9b920b5
Javier Esteban Gonzalez Lopez <gonzalezlopezjavieresteban@gmail.com>
2024-08-23 11:45:30 -0500

    nuevos cambios generados en el index y en el package.json

--- src/indice.js
+++ src/indice.js
@@ -2,9 +2,9 @@ const bodyParser = require('body-parser'); // Body-parser permite manejar datos 
 
 const { layout } = require('ejs-mate'); // Se usa para ejecutar la plantilla que permite incrustar código JavaScript dentro de HTML para generar páginas dinámicas.
 
-const express = require('express'); // Llama al Framework Express para crear el servidor
+const express = require('express'); // Llama al Framework Express para crear el servidor wed
 
-const app = express(); // Renombrado para mayor claridad
+const puerto= express(); // Renombrado para mayor claridad
 //
 const exphbs = require('express-handlebars'); // Motor de plantillas para formularios con Handlebars para Express.
 
@@ -12,18 +12,18 @@ const path = require('path'); // Módulo de Node.js para manejar y transformar r
 
 require('./database'); // Configuración del puerto
 
-app.set('escuchar', 9091); // Definición del puerto por donde se ingresa al servidor
+puerto.set('escuchar', 5000); // Definición del puerto por donde se ingresa al servidor
 
-app.use(express.json()); // Maneja las solicitudes JSON.
+puerto.use(express.json()); // Maneja las solicitudes JSON.
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // Maneja datos enviados desde formularios HTML.
+puerto.use(bodyParser.json());//formato de texto
+puerto.use(bodyParser.urlencoded({ extended: true })); // Maneja datos enviados desde formularios HTML.
 
-app.set('views', path.join(__dirname, 'vistas')); // Establece la ruta de la carpeta de vistas donde estarán los archivos de plantillas Handlebars.
+puerto.set('views', path.join(__dirname, 'vistas')); // Establece la ruta de la carpeta de vistas donde estarán los archivos de plantillas Handlebars.
 
-app.engine(
-    "hbs",
-    exphbs({
+puerto.engine(
+    "hbs",//procesador de plantillas.
+    exphbs.engine({
         extname: "hbs", // Define la extensión de los archivos de plantilla.
 
         defaultLayout: false, // No usa un diseño predeterminado.
@@ -32,10 +32,13 @@ app.engine(
     })
 );
 
-app.set('view engine', '.hbs'); // Define Handlebars como el motor de vistas con la extensión ".hbs".
+puerto.set('view engine', '.hbs'); // Define Handlebars como el motor de vistas con la extensión ".hbs".
+//son dos clases de puerto fisicos(de la maquina) y logicos(de la red)
+//->para difinir una funcion
 
-app.listen(app.get('escuchar'), () => {
-    console.log('Servidor conectado en el puerto', app.get('escuchar'));
+puerto.listen(puerto.get('escuchar'), () => {//Escuchar es una variable que contiene el nombre del puerto.
+    console.log('Servidor conectado en el puerto', puerto.get('escuchar'));
 }); // Inicia el servidor en el puerto especificado.
 
-app.use(require('./rutas/index')); // Importa y usa las rutas definidas en el archivo index.js dentro de la carpeta rutas.
+puerto.use(require('./rutas/index')); // Importa y usa las rutas definidas en el archivo index.js dentro de la carpeta rutas.
+ 
